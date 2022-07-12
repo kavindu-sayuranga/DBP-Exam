e7b6dedca95c2f14dcce70790fb2f949fc5ba511
kavindu-sayuranga <ksayuranga00@gmai.com>
2022-07-12 10:47:38 +0530

    Connect Database

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class StudentFormController {
+    public JFXTextField txtStudentID;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNIC;
+    public TableView tblStudent;
+    public TableColumn colStudentID;
+    public TableColumn colStudentName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNIC;
+
+
+
+    public void btnAddStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuDeleteOnAction(ActionEvent actionEvent) {
+    }
 }
