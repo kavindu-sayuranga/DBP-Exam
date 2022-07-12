e7a23e553fa454bfa7f37d7b9e0ba59ec5aad1a4
kavindu-sayuranga <ksayuranga00@gmai.com>
2022-07-12 11:28:50 +0530

    add student

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,9 +1,19 @@
 package controller;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Student;
+import util.CrudUtil;
+import views.tm.StudentTM;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StudentFormController {
     public JFXTextField txtStudentID;
@@ -12,7 +22,7 @@ public class StudentFormController {
     public JFXTextField txtContact;
     public JFXTextField txtAddress;
     public JFXTextField txtNIC;
-    public TableView tblStudent;
+    public TableView<StudentTM> tblStudent;
     public TableColumn colStudentID;
     public TableColumn colStudentName;
     public TableColumn colEmail;
@@ -21,9 +31,21 @@ public class StudentFormController {
     public TableColumn colNIC;
 
 
-
     public void btnAddStudentOnAction(ActionEvent actionEvent) {
+        Student stu = new Student(
+                txtStudentID.getText(),txtStudentName.getText(), txtEmail.getText(),txtContact.getText(),txtAddress.getText(),txtNIC.getText()
+        );
+
+        try {
+            if (CrudUtil.execute("INSERT INTO Student VALUES (?,?,?,?,?,?)",stu.getId(),stu.getName(),stu.getEmail(),stu.getContact(),stu.getAddress(),stu.getNic())){
+                new Alert(Alert.AlertType.CONFIRMATION, "Saved!..").show();
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
+
 
     public void menuUpdateOnAction(ActionEvent actionEvent) {
     }
